3b74bf063228cd9c7675598f6ad0a3c8018f15fd
brianthomashammond <brianthomashammond@gmail.com>
2020-10-20 14:15:59 -0700

    read ready for pr

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Card, CardTitle, Col } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 export default class CatIndex extends Component {
     render() {
         return (
             <>
                 <h3>Cat Index</h3>
+                <br />
+                <Col sm="6">
+                    {this.props.cats.map((cat, index) => {
+                        return (
+                            <Card key={index}>
+                                <CardTitle>
+                                    <NavLink to={`/catshow/${cat.id}`}>
+                                        {cat.name}
+                                    </NavLink>
+                                </CardTitle>
+                            </Card>
+                        )
+                    })}
+                </Col>
             </>
         )
     }
