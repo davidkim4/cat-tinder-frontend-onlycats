f6539fb242f3d82a0938c063fe3be70ee742a50d
davidkim4 <dkim4016@gmail.com>
2020-10-20 12:01:19 -0700

    added some styling header and not found

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import {
+    Collapse,
+    Jumbotron,
+    Container,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
 
-export default class Header extends Component {
-    render() {
-        return(
-            <>
-                <h1>Header</h1>
-            </>
-        )
-    }
-}
+// export default class Header extends Component {
+const Header = (props) => {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
+    // render() {
+    return (
+        <div>
+            <Jumbotron fluid>
+                <Container fluid>
+                    <h1 className="display-3">OnlyCats</h1>
+                    <p className="lead">This is a site that only has cats on it... WHAT ELSE WERE YOU EXPECTING?!</p>
+                    <Navbar color="faded" light>
+                        <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+                        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+                        <Collapse isOpen={!collapsed} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink href="/components/">Components</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+                </Container>
+            </Jumbotron>
+        </div>
+    );
+};
+export default Header;
+//     }
+// }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import doge from "../assets/doge.png";
 
 export default class NotFound extends Component {
     render() {
         return (
             <>
-                <h3>Not Found</h3>
+                <img src={doge} alt="Majestic dog" />
+                <h3>You seem to be lost...</h3>
             </>
         )
     }
