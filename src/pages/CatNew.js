c9aebc8efff0787eb1d55cd37384a119725258ca
DiemBTran <diembtran@gmail.com>
2020-10-21 11:23:33 -0700

    added create a cat functionality

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Redirect } from 'react-router-dom'
 
 export default class CatNew extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: {
+                name: "",
+                age: "",
+                enjoys: ""
+            },
+            success: false
+        }
+    }
+
+    handleChange = (e) => {
+        let { form } = this.state
+        form[e.target.name] = e.target.value
+        this.setState({ form: form })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.createNewCat(this.state.form)
+        this.setState({ success: true })
+    }
+
     render() {
         return (
             <>
                 <h3>Cat New</h3>
+                <Form>
+                    <FormGroup>
+                        <Label for="name">Cat's Name</Label>
+                        <Input type="text" name="name" onChange={this.handleChange}
+                            value={this.state.form.name} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="age">Cat's Age</Label>
+                        <Input type="number" name="age" onChange={this.handleChange}
+                            value={this.state.form.age} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="enjoys">What does your cat enjoy doing?</Label>
+                        <Input type="text" name="enjoys" onChange={this.handleChange}
+                            value={this.state.form.enjoys} />
+                    </FormGroup>
+                    <Button name="submit" onClick={this.handleSubmit}>
+                        Add a Cat
+                    </Button>
+                </Form>
+                { this.state.success && <Redirect to="/catindex" />}
             </>
         )
     }
