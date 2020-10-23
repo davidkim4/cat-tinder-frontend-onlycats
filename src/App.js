e1fd1938691f0f1e6f04ec561898ca80ee576a5b
davidkim4 <dkim4016@gmail.com>
2020-10-23 10:48:50 -0700

    some styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CatNew from './pages/CatNew.js';
 import CatEdit from './pages/CatEdit.js';
 import NotFound from './pages/NotFound.js';
 import mockCats from './mockCats.js'
-
+import './index.css'
 
 import {
   BrowserRouter as Router,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-import catGroup from "../assets/catGroup.jpg";
+import cats from "../assets/cats.png";
 
 export default class Home extends Component {
     render() {
         return (
             <>
-                <img src={catGroup} alt="a group of kittens" />
+                <div id="homeRow">
+                    <div id="enter">
+                        <a href="/catindex"><button class="button">Look At Cats!</button></a>
+                    </div>
+                    <img id="homeImg" src={cats} alt="a group of kittens" />
+                </div>
+                <div id="impawsible">
+                    <p>Impawsible... It's all just cat photos?</p>
+                    <p>Always nyas been...</p>
+                </div>
+                <div id="info">
+                    <div id="post">
+                        <div id="title">
+                            <h2>Post<i class="material-icons">publish</i></h2>
+                        </div>
+                        <hr></hr>
+                        <div>
+                            <p>Purr-ticipate in a network of feline enthusiasts and upload photos of your adorable cat so that the whole world can see!</p>
+                        </div>
+                    </div>
+                    <div id="like">
+                        <div id="title">
+                            <h2>Like<i class="material-icons">favorite</i></h2>
+                        </div>
+                        <hr></hr>
+                        <div>
+                            <p>We’re not kitt-ing! Share your love by liking photos of other user’s cats and show your fur-iends! </p>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
